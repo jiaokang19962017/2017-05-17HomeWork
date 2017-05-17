2f7ac7adf0cd527475301c918a6816b9ff45d087
jiaoakng <Administrator@HP201-1>
2017-05-17 15:14:47 +0800

    作业Done

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,10 +6,78 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+
+    enum Weekday { Sunday, Monday = 2, Tuesday, Wednesday, Thursday, Friday, Staturday };
+    public struct Employee
+    {
+        public int id;
+        public string name;
+        public int age;
+        public char sex;
+        public string phone;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
+            #region 1.
+            /* int flag = Convert.ToInt32(Console.ReadLine());
+         Weekday week = (Weekday)flag;
+         switch (week)
+         {
+             case Weekday.Sunday:
+                 Console.WriteLine("sunday");
+                 break;
+             case Weekday.Monday:
+                 Console.WriteLine("monday");
+                 break;
+             case Weekday.Tuesday:
+                 Console.WriteLine("tuesday");
+                 break;
+             case Weekday.Wednesday:
+                 Console.WriteLine("wednesday");
+                 break;
+             case Weekday.Thursday:
+                 Console.WriteLine("thursday");
+                 break;
+             case Weekday.Friday:
+                 Console.WriteLine("friday");
+                 break;
+             case Weekday.Staturday:
+                 Console.WriteLine("staturday");
+                 break;
+         } */
+
+
+            /* foreach (string item in Enum.GetNames(typeof(Weekday)))
+             {
+                 Console.WriteLine(item);
+             } */
+
+
+            /* foreach (int item in Enum.GetValues(typeof(Weekday)))
+             {
+                 Console.WriteLine(item);
+             }*/
+            #endregion
+
+
+            #region 2.
+
+            Employee emp;
+            emp.id = 1;
+            emp.name = "jiaokang";
+            emp.age = 21;
+            emp.sex = '男';
+            emp.phone = "17853753772";
+
+            Console.WriteLine(emp.id);
+            Console.WriteLine(emp.name);
+            Console.WriteLine(emp.age);
+            Console.WriteLine(emp.sex);
+            Console.WriteLine(emp.phone);
+            #endregion
         }
     }
 }
